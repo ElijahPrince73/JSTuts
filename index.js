afcb617e0248ed65d9afadf056ed3c95a245f1fa
elijahprince73 <elijahprince73@gmail.com>
2017-08-01 10:48:02 -0700

    varibles lecture done

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var name = 'john';
-var age = 26;
+// Variables
 
-var job, isMarirred;
+// var name = 'john';
+// var age = 26;
+//
+// var job, isMarirred;
+//
+// job = 'teacher';
+// isMarirred = false;
+// console.log(name + ' is a ' + age + ' year old ' + job + ' is he married ' + isMarirred);
+//
+// age = 'thirty six';
+// job = 'driver'
+//
+// console.log(name + ' is a ' + age + ' year old ' + job + ' is he married ' + isMarirred);
+//
+// var lasName = prompt('What is the last name')
+// console.log(lasName);
 
-job = 'teacher';
-isMarirred = false;
-console.log(name + ' is a ' + age + ' year old ' + job + ' is he married ' + isMarirred);
-
-age = 'thirty six';
-job = 'driver'
-
-console.log(name + ' is a ' + age + ' year old ' + job + ' is he married ' + isMarirred);
+//Operators
