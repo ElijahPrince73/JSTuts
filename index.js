b2e623b07862534295a26211735de8ee9aa8152e
elijahprince73 <elijahprince73@gmail.com>
2017-08-01 10:29:52 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith'
+
+console.log(name + " " + lastName);
+
+var age = 26;
+
+console.log(age);
+
+var fullAge = true;
+
+console.log(fullAge);
