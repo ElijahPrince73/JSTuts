86bcd5cdba4b269a1746e6ce5a3e814ca293ebde
elijahprince73 <elijahprince73@gmail.com>
2017-08-07 16:37:36 -0700

    Primitivs and objects

--- Lecture-3/script.js
+++ Lecture-3/script.js
@@ -1,13 +1,13 @@
 // Function constructor
 
-var john = {
-  name: 'John',
-  yearOfBirth: 1999,
-  job: 'teacher'
-}
+// var john = {
+//   name: 'John',
+//   yearOfBirth: 1999,
+//   job: 'teacher'
+// }
 
 //Constructor
-var Person = function(name, yearOfBirth, job) {
+/*var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
@@ -31,11 +31,11 @@ mark.calculateAge()
 
 console.log(john.lastName);
 console.log(jane.lastName);
-console.log(mark.lastName);
+console.log(mark.lastName);*/
 
 //Testing
 
-var Spaceship = function(type, lightSpeedLimit, crewLimit) {
+/*var Spaceship = function(type, lightSpeedLimit, crewLimit) {
   this.type = type
   this.lightSpeedLimit = lightSpeedLimit;
   this.crewLimit = crewLimit
@@ -51,4 +51,55 @@ console.log(burrito);
 
 console.log(deathStar.Appears);
 
-console.log(deathStar);
+console.log(deathStar);*/
+
+//Object.create
+
+/*var personProto = {
+  calculateAge: function () {
+    console.log(2017 - this.yearOfBirth);
+  }
+}
+
+var john = Object.create(personProto)
+
+console.log(john);
+
+john.name = 'john'
+john.yearOfBirth = 1899
+john.job = 'teacher'
+
+var jane = Object.create(personProto, {
+  name: {value: 'jane'},
+  yearOfBirth:{value: 1997},
+  job: {value:'Designer'}
+})
+
+console.log(jane);*/
+
+// Primitive vs Objects
+
+var a = 23;
+
+var b = a
+
+a = 46
+
+console.log(a);
+console.log(b);
+
+var obj1 = {
+  name: 'john',
+  age: 26
+}
+
+var obj2 = {
+  name: 'john',
+  age: 26
+}
+
+var obj2 = obj1;
+
+obj.age = 3
+
+console.log();
