2d8dee107390d82c0dd96881873aa36861a72e9d
Elijah Prince <elijahprince73@gmail.com>
2017-08-21 21:02:22 -0700

    es6 arrays

--- ES2015/script.js
+++ ES2015/script.js
@@ -249,3 +249,50 @@ box66.clickMe()*/
 // console.log(retirement);
 
 //Lecture: Arrays
+
+// const boxes = document.querySelectorAll('.box');
+
+//ES5
+// var boxesArr = Array.prototype.slice.call(boxes)
+// boxesArr.forEach(function(cur) {
+//   cur.style.backgroundColor = 'dodgerblue'
+// })
+
+//ES6
+// const boxesArr6 = Array.from(boxes);
+//
+// Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue')
+
+//ES5
+// for (var i = 0; i < boxesArr.length; i++) {
+//   if (boxesArr[i].className === 'box blue') {
+//     continue;
+//   }
+//   boxesArr[i].textContent = ' I changed to blue'
+// };
+
+//ES6
+// for (const cur of boxesArr6) {
+//   if (cur.className.includes('blue')) {
+//     continue;
+//   }
+//   cur.textContent = ' I changed to blue'
+// }
+
+//ES5
+var ages = [12, 21, 3, 44, 66]
+
+var full = ages.map(function(cur) {
+  return cur >= 18
+})
+
+// console.log(full);
+//
+// console.log(full.indexOf(true));
+// console.log(ages[full.indexOf(true)]);
+
+//ES6
+
+console.log(ages.findIndex(cur => cur >= 18));
+
+console.log(ages.find(cur => cur >= 18));
