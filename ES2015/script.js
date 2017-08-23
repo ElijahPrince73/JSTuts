baa0dc53567508beabdc5d824958ec32a129335b
Elijah Prince <elijahprince73@gmail.com>
2017-08-22 23:30:07 -0700

    challenge

--- ES2015/script.js
+++ ES2015/script.js
@@ -339,7 +339,6 @@ box66.clickMe()*/
 //
 // Array.from(all).forEach(cur => cur.style.color = 'purple')
 
-
 //////////////////////////////////////////////////////////////////////Lecture: Rest Paremeters
 
 //ES5
@@ -499,7 +498,6 @@ box66.clickMe()*/
 //
 // Person6.greeting()
 
-
 ////////////////////////////////////////////////////////Lecture: subclasses
 
 //ES5
@@ -562,5 +560,31 @@ box66.clickMe()*/
 //
 // const johnAthlete6 = new Athlete6('john', 1990, 'swimmer', 3, 10)
 //
+// console.log(johnAthlete6);
+//
 // johnAthlete6.wonMedal()
 // johnAthlete6.calcAge()
+
+/////////////////////////////////
+// CODING CHALLENGE
+
+/*
+
+Suppose that you're working in a small town administration, and you're in charge of two town elements:
+1. Parks
+2. Streets
+
+It's a very small town, so right now there are only 3 parks and 4 streets. All parks and streets have a name and a build year.
+
+At an end-of-year meeting, your boss wants a final report with the following:
+1. Tree density of each park in the town (forumla: number of trees/park area)
+2. Average age of each town's park (forumla: sum of all ages/number of parks)
+3. The name of the park that has more than 1000 trees
+4. Total and average length of the town's streets
+5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal
+
+All the report data should be printed to the console.
+
+HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+
+*/
